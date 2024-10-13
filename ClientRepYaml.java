b5c50d58c5e6a5c80f0d77b3856ec88c52cccdaa
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-10-13 18:26:53 +0300

    laba2: changing ClientRepYaml which inherits from ClientRep

--- ClientRepYaml.java
+++ ClientRepYaml.java
@@ -1,12 +1,8 @@
-class ClientRepYaml {
-    private String filename;
-    private List<Client> clients;
-
+class ClientRepYaml extends ClientRep{
     public ClientRepYaml(String filename) {
-        this.filename = filename;
-        this.clients = readAll();
+        super(filename);
     }
-
+    @Override
     public List<Client> readAll() {
         List<Client> clients = new ArrayList<>();
         File file = new File(filename);
@@ -29,21 +25,23 @@ class ClientRepYaml {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         for (Client client : clients) {
             System.out.println(client.toYaml());
         }
         return clients;
     }
 
-    private void saveAll() {
+
+    @Override
+    protected void saveAll() {
         DumperOptions options = new DumperOptions();
-        options.setIndent(4); // Установка отступа
+        options.setIndent(4);
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
         options.setPrettyFlow(true);
 
         Representer representer = new Representer(options);
-        representer.addClassTag(Client.class, Tag.MAP);
+        representer.addClassTag(Client.class, Tag.MAP); 
 
         Yaml yaml = new Yaml(representer, options);
 
@@ -52,81 +50,5 @@ class ClientRepYaml {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public Client getById(int clientId) {
-        for (Client client : clients) {
-            if (client.getId() == clientId) {
-                return client;
-            }
-        }
-        return null;
-    }
-
-    public List<Client> getKNSortList(int k, int n) {
-        int start = (k - 1) * n;
-        int end = Math.min(start + n, clients.size());
-        return clients.subList(start, end);
-    }
-
-    public void sortByField() {
-        clients.sort(Comparator.comparing(Client::getId));
-    }
-
-    private boolean isUnique(String phone) {
-        for (Client client : clients) {
-            if (client.getPhone().equals(phone)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void addClient(String name, String surname, String patronymic, String phone, String email, String gender) throws Exception {
-        addClient(name, surname, patronymic, 0, phone, email, gender); // Присваиваем 0 по умолчанию
-    }
-
-    // Добавление объекта (формируется новый ID)
-    public void addClient(String name, String surname, String patronymic, Integer total_services, String phone, String email, String gender) throws Exception {
-        int newId = clients.stream().mapToInt(Client::getId).max().orElse(0) + 1;
-        if (!isUnique(phone)) {
-            throw new Exception("Клиент с таким телефоном уже существует!");
-        }
-        Client newClient = new Client(newId, name, surname, patronymic, total_services, phone, email, gender);
-        clients.add(newClient);
-        saveAll();
-    }
-
-    public boolean replaceById(int clientId, Client newClient) throws Exception {
-        for (int i = 0; i < clients.size(); i++) {
-            if (clients.get(i).getId() == clientId) {
-                if (!isUnique(newClient.getPhone())) {
-                    throw new Exception("Нельзя заменить клиента: клиент с таким телефоном уже существует!");
-                }
-                Client client;
-                if (newClient.getId() == null && newClient.getServices() == null) {
-                    client = new Client(clientId, newClient.getName(), newClient.getSurname(), newClient.getPatronymic(), clients.get(i).getServices(), newClient.getPhone(), newClient.getEmail(), newClient.getGender());
-                } else if (newClient.getId() == null) {
-                    client = new Client(clientId, newClient.getName(), newClient.getSurname(), newClient.getPatronymic(), newClient.getServices(), newClient.getPhone(), newClient.getEmail(), newClient.getGender());
-                } else {
-                    client = newClient;
-                }
-                clients.set(i, client);
-                saveAll();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void deleteById(int clientId) {
-        clients = clients.stream()
-                .filter(customer -> customer.getId() != clientId)
-                .collect(Collectors.toList());
-        saveAll();
-    }
-
-    public int getCount() {
-        return clients.size();
     }
 }
