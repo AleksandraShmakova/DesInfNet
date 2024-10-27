4176fa8b910fb53af783bf888fee9b79a87c34bf
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-10-27 22:28:57 +0300

    laba2: delete IClientRep.java

--- IClientRep.java
+++
@@ -1,8 +0,0 @@
-interface IClientRep {
-    Client getById(int clientId);
-    List<Client> getKNSortList(int k, int n);
-    void addClient(Client client) throws Exception;
-    boolean replaceById(int clientId, Client newClient) throws Exception;
-    void deleteById(int clientId);
-    int getCount();
-}
