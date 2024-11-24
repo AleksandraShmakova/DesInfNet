dd64e001118fbecc0e19ecc71805c7357dba240b
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-11-24 17:53:30 +0300

    lab3: update IClientRepository.java (adding updateClient)

--- IClientRepository.java
+++ IClientRepository.java
@@ -2,7 +2,7 @@ interface IClientRepository {
     Client getById(int clientId);
     List<Client> getKNSortList(int k, int n);
     void addClient(Client client) throws Exception;
-    boolean replaceById(int clientId, Client newClient) throws Exception;
+    boolean updateClient(int clientId, Client newClient) throws Exception;
     void deleteById(int clientId);
     int getCount();
 
