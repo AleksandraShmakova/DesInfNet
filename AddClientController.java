cb4c022cc5d8c85cdae79c6497cbd119d395106d
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-12-05 12:50:50 +0300

    lab3: fixing addController

--- AddClientController.java
+++ AddClientController.java
@@ -1,28 +1,20 @@
 class AddClientController {
     private IClientRepository clientRepository;
     private ClientFormView formView;
-    private ClientObserver mainController;
 
-    public AddClientController(IClientRepository clientRepository, ClientController mainController) {
+    public AddClientController(IClientRepository clientRepository) {
         this.clientRepository = clientRepository;
         this.formView = new ClientFormView("Добавить клиента", "Добавить", this);
-        this.mainController = mainController;
-
         formView.setVisible(true);
     }
 
     public void onConfirmButtonClick() {
         Client clientInput = formView.getClientInput();
-        if (isValidClient(clientInput)) {
-            try {
-                clientRepository.addClient(clientInput);
-                mainController.update(clientRepository.getKNSortList(1, clientRepository.getCount()));
-                formView.dispose();
-            } catch (Exception ex) {
-                formView.showError("Ошибка при добавлении клиента: " + ex.getMessage());
-            }
-        } else {
-            formView.showError("Некорректные данные клиента!");
+        try {
+            clientRepository.addClient(clientInput);
+            formView.dispose();
+        } catch (Exception ex) {
+            //formView.showError("Ошибка при добавлении клиента!");
         }
     }
 
@@ -30,12 +22,10 @@ class AddClientController {
         formView.dispose();
     }
 
-    private boolean isValidClient(Client client) {
-
-        if (client.getSurname().trim().isEmpty() || client.getName().trim().isEmpty() || client.getPatronymic().trim().isEmpty() || client.getEmail().trim().isEmpty() || !client.getEmail().matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$") || client.getGender().trim().isEmpty()) {
-            return false;
-        }
-        return true;
+    public  boolean isValid(String surname, String name, String patr, int total_services, String phone, String email, String gender) {
+        if(Client.validateS(surname) && Client.validateS(name) && Client.validateS(patr) && Client.validateI(total_services) && Client.validatePhone(phone) && Client.validateEmail(email) && Client.validateS(gender))
+            return true;
+        return false;
     }
 
 }
