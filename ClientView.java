dfb0d5dda70b2c7dce3063b0fa00abcf1586c2cc
AleksandraShmakova <113943664+AleksandraShmakova@users.noreply.github.com>
2024-12-13 13:15:50 +0300

    lab4: update view for the shared presenter

--- ClientView.java
+++ ClientView.java
@@ -3,8 +3,10 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.List;
-class ClientView extends JFrame {
+
+class ClientView extends JFrame implements IClientView {
     private JTable clientTable;
     private JTextField surnameField;
     private JTextField nameField;
@@ -14,17 +16,16 @@ class ClientView extends JFrame {
     private JTextField emailField;
     private JTextField genderField;
     private JButton addButton;
+    private JButton cancelButton;
     private JButton deleteButton;
     private JButton updateButton;
-    //private JButton refreshButton;
     private JButton moreInfoButton;
     private JButton nextPageButton;
     private JButton prevPageButton;
-    private ClientController controller;
+    private ClientPresenter presenter;
 
 
-    public ClientView(ClientController controller) {
-        this.controller = controller;
+    public ClientView() {
         // Настройка окна
         setTitle("Client Management");
         setSize(600, 330);
@@ -44,13 +45,11 @@ class ClientView extends JFrame {
         JPanel buttonPanel = new JPanel(new FlowLayout());
         prevPageButton = new JButton("Назад");
         addButton = new JButton("Добавить");
-        //refreshButton = new JButton("Обновить список");
         moreInfoButton = new JButton("Подробнее");
         nextPageButton = new JButton("Вперед");
 
         buttonPanel.add(prevPageButton);
         buttonPanel.add(addButton);
-        //buttonPanel.add(refreshButton);
         buttonPanel.add(moreInfoButton);
         buttonPanel.add(nextPageButton);
 
@@ -59,27 +58,15 @@ class ClientView extends JFrame {
 
         add(controlPanel, BorderLayout.SOUTH);
 
-        addButton.addActionListener(e -> controller.onAddButtonClick());
-        //refreshButton.addActionListener(e -> controller.onRefreshButtonClick());
-        moreInfoButton.addActionListener(e -> controller.onMoreInfoButtonClick());
-        nextPageButton.addActionListener(e -> controller.onNextPageClick());
-        prevPageButton.addActionListener(e -> controller.onPrevPageClick());
+        addButton.addActionListener(e -> generateAddClientPage());
+        moreInfoButton.addActionListener(e -> onMoreInfoButtonClick());
         display();
     }
-    // Кастомная модель для таблицы, с запретом редактирования определенных колонок
-    private static class ClientTableModel extends DefaultTableModel {
-        @Override
-        public boolean isCellEditable(int row, int column) {
-            return column != 0 && column != 1 && column != 2 && column != 3;
-        }
-    }
 
-    public JTable getClientTable() {
-        return clientTable;
-    }
-
-    // Обновление представления с данными клиентов
-    public void update(List<Client> clients, int offset) {
+    @Override
+    public String update(List<Client> clients, int currentPage, int recordsPerPage, int totalPages) {
+        //int totalPages = (int) Math.ceil((double) clients.size() / recordsPerPage);
+        int offset = (currentPage - 1) * recordsPerPage;
         String[] columnNames = {"№", "Фамилия", "Имя", "Телефон"};
         ClientTableModel model = (ClientTableModel) clientTable.getModel();
         model.setColumnIdentifiers(columnNames);
@@ -95,19 +82,136 @@ class ClientView extends JFrame {
             };
             model.addRow(row);
         }
-    }
 
-    public void updatePaginationControls(int currentPage, int totalPages) {
+        nextPageButton.addActionListener(e -> {
+            try {
+                onNextPageClick(currentPage, totalPages);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        prevPageButton.addActionListener(e -> {
+            try {
+                onPrevPageClick(currentPage);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
         prevPageButton.setEnabled(currentPage > 1);
         nextPageButton.setEnabled(currentPage < totalPages);
+        return null;
     }
 
-    // Метод для отображения сообщений об ошибках
-    public void showError(String message) {
+    public void onNextPageClick(int currentPage, int totalPages) throws IOException {
+        if (currentPage < totalPages) {
+            currentPage++;
+            presenter.setCurrentPage(currentPage);
+            presenter.update();
+        }
+    }
+
+    public void onPrevPageClick(int currentPage) throws IOException {
+        if (currentPage > 1) {
+            currentPage--;
+            presenter.setCurrentPage(currentPage);
+            presenter.update();
+        }
+    }
+
+    private static class ClientTableModel extends DefaultTableModel {
+        @Override
+        public boolean isCellEditable(int row, int column) {
+            return column != 0 && column != 1 && column != 2 && column != 3;
+        }
+    }
+
+    @Override
+    public void setPresenter(ClientPresenter presenter) {
+        this.presenter = presenter;
+        try {
+            presenter.update();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void onMoreInfoButtonClick() {
+        int selectedRow = clientTable.getSelectedRow();
+        if (selectedRow != -1) {
+            try {
+                String phone = (String) clientTable.getValueAt(selectedRow, 3);
+                int clientId = presenter.getClientId(phone);
+                presenter.getClientDetailsPage(clientId);
+            } catch (Exception e) {
+                showErrorMessage("Ошибка при нахождении клиента: " + e.getMessage());
+            }
+        } else {
+            showErrorMessage("Выберите клиента для просмотра информации.");
+        }
+    }
+
+    public void onAddButtonClick(JFrame infoWindow) throws Exception {
+        Client newClient = getInfo(infoWindow);
+        presenter.onAddButtonClick(newClient);
+        presenter.update();
+        infoWindow.dispose();
+    }
+
+    @Override
+    public String generateAddClientPage() {
+        JFrame infoWindow = new JFrame("Добавление клиента");
+        infoWindow.setSize(400, 300);
+        infoWindow.setLayout(new GridLayout(8, 2));
+
+        // Добавляем метки с информацией
+        infoWindow.add(new JLabel("Фамилия: "));
+        surnameField = new JTextField();
+        infoWindow.add(surnameField);
+        infoWindow.add(new JLabel("Имя: "));
+        nameField = new JTextField();
+        infoWindow.add(nameField);
+        infoWindow.add(new JLabel("Отчество: "));
+        patrField = new JTextField();
+        infoWindow.add(patrField);
+        infoWindow.add(new JLabel("Количество стрижек: "));
+        servicesField = new JTextField();
+        infoWindow.add(servicesField);
+        infoWindow.add(new JLabel("Телефон: "));
+        phoneField = new JTextField();
+        infoWindow.add(phoneField);
+        infoWindow.add(new JLabel("Email: "));
+        emailField = new JTextField();
+        infoWindow.add(emailField);
+        infoWindow.add(new JLabel("Пол: "));
+        genderField = new JTextField();
+        infoWindow.add(genderField);
+
+        addButton = new JButton("Добавить");
+        cancelButton = new JButton("Отмена");
+        infoWindow.add(addButton);
+        infoWindow.add(cancelButton);
+
+        addButton.addActionListener(e -> {
+            try {
+                onAddButtonClick(infoWindow);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        cancelButton.addActionListener(e -> infoWindow.dispose());
+
+        // Настраиваем и показываем окно
+        infoWindow.setLocationRelativeTo(this);
+        infoWindow.setVisible(true);
+        return null;
+    }
+
+    @Override
+    public void showErrorMessage(String message) {
         JOptionPane.showMessageDialog(this, message, "Ошибка", JOptionPane.ERROR_MESSAGE);
     }
-
-    public void openInfoWindow(Client client) {
+    @Override
+    public String displayClientDetails(Client client) {
         JFrame infoWindow = new JFrame("Информация о клиенте");
         infoWindow.setSize(400, 300);
         infoWindow.setLayout(new GridLayout(8, 2));
@@ -123,7 +227,7 @@ class ClientView extends JFrame {
         patrField = new JTextField(client.getPatronymic());
         infoWindow.add(patrField);
         infoWindow.add(new JLabel("Количество стрижек: "));
-        servicesField = new JTextField(String.valueOf(client.getServices()));
+        servicesField = new JTextField(String.valueOf(client.getTotal_services()));
         infoWindow.add(servicesField);
         infoWindow.add(new JLabel("Телефон: "));
         phoneField = new JTextField(client.getPhone());
@@ -142,21 +246,37 @@ class ClientView extends JFrame {
         infoWindow.add(updateButton);
 
         String phone = phoneField.getText();
-        deleteButton.addActionListener(e -> controller.onDeleteButtonClick(phone, infoWindow));
-        updateButton.addActionListener(e -> getInfo(infoWindow));
+        deleteButton.addActionListener(e -> onDeleteButtonClick(phone, infoWindow));
+        updateButton.addActionListener(e -> onUpdateButtonClick(infoWindow));
 
         // Настраиваем и показываем окно
         infoWindow.setLocationRelativeTo(this);
         infoWindow.setVisible(true);
+        return null;
     }
 
-    public void getInfo(JFrame infoWindow) {
-        if(!controller.isValid(surnameField.getText(), nameField.getText(), patrField.getText(), Integer.parseInt(servicesField.getText()), phoneField.getText(), emailField.getText(), genderField.getText())) {
-            showError("Некорректно заполнены данные!");
+    public void onDeleteButtonClick(String phone, JFrame infoWindow) {
+        int clientId = presenter.getClientId(phone);
+        presenter.onDeleteButtonClick(clientId);
+        infoWindow.dispose();
+    }
+
+    public void onUpdateButtonClick(JFrame infoWindow) {
+        Client updatedClient = getInfo(infoWindow);
+        int clientId = presenter.getClientId(updatedClient.getPhone());
+        updatedClient.setId(clientId);
+        presenter.onUpdateButtonClick(updatedClient);
+        infoWindow.dispose();
+    }
+
+    public Client getInfo(JFrame infoWindow) {
+        if(!presenter.isValid(surnameField.getText(), nameField.getText(), patrField.getText(), Integer.parseInt(servicesField.getText()), phoneField.getText(), emailField.getText(), genderField.getText())) {
+            showErrorMessage("Некорректно заполнены данные!");
+            return null;
         }
         else {
-            Client updatedClient = new Client(surnameField.getText(), nameField.getText(), patrField.getText(), Integer.parseInt(servicesField.getText()), phoneField.getText(), emailField.getText(), genderField.getText());
-            controller.onUpdateButtonClick(updatedClient, infoWindow);
+            Client client = new Client(surnameField.getText(), nameField.getText(), patrField.getText(), Integer.parseInt(servicesField.getText()), phoneField.getText(), emailField.getText(), genderField.getText());
+            return client;
         }
     }
 
